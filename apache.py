ed4c0b98ede066dcd5bfa26dca91bbf08a381516
kvrs <43546720+kvrs@users.noreply.github.com>
2024-06-12 23:06:05 +0530

    Update apache.py

--- apache.py
+++ apache.py
@@ -4,10 +4,10 @@ def run():
     p1 = beam.Pipeline()
     voos = (
         p1
-        | "Read Data" >> beam.io.ReadFromText("gs://<your-bucket-name>/california_housing_test.csv", skip_header_lines=1)
+        | "Read Data" >> beam.io.ReadFromText("gs://dagoutput/california_housing_test.csv", skip_header_lines=1)
         | "Split by comma" >> beam.Map(lambda record: record.split(','))
         | "Print Results" >> beam.Map(print)
-        | "Write Results" >> beam.io.WriteToText("gs://<your-bucket-name>/california_housing_test_raj.csv")
+        | "Write Results" >> beam.io.WriteToText("gs://dagoutput/california_housing_test_raj.csv")
     )
     p1.run().wait_until_finish()
 
