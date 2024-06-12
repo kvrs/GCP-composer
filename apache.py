b0a4e9591bcbbbda3cc8ecc2cb495a5432adfc41
kvrs <43546720+kvrs@users.noreply.github.com>
2024-06-12 21:08:41 +0530

    Update apache.py

--- apache.py
+++ apache.py
@@ -1,2 +1,10 @@
-# create beam template 
-print('hello world')
+import apache_beam as beam
+
+p1 = beam.Pipeline()
+voos =(p1
+       | "import Data" >> beam.io.ReadFromText("california_housing_test.csv", skip_header_lines=1)
+       | "Split by comma" >> beam.Map(lambda record: record.split(','))
+       |"Print Results" >> beam.Map(print)
+       | "Write Results" >> beam.io.WriteToText("gs://dagoutput/california_housing_test_raj.csv")
+      )
+p1.run()
