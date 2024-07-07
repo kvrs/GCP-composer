03742bea78ccd949dea9389c47fbb177083da3b2
kvrs <43546720+kvrs@users.noreply.github.com>
2024-07-07 21:29:24 +0530

    Update process2.py (#22)

--- Process_2/process2.py
+++ Process_2/process2.py
@@ -9,3 +9,7 @@ Require status checks before merging
 Require status checks before merging
 Require status checks before merging
 Require status checks before merging
+#Require status checks before mergingRequire status checks before merging Require status checks before merging
+#Require status checks before merging
+#Require status checks before merging
+
