8c85a33aaae4ab42a5b851b20761c8ee53227188
kvrs <43546720+kvrs@users.noreply.github.com>
2024-07-07 21:23:23 +0530

    Pre prod (#20)
    * Dev (#6) (#10) (#15)
    
    * Create process.py (#4)
    
    * Create process2.py (#5)
    
    Co-authored-by: malyalarajasekhar <118497522+malyalarajasekhar@users.noreply.github.com>
    
    * Update process.py (#17)
    
    * Update process2.py (#18)
    
    ---------
    
    Co-authored-by: malyalarajasekhar <118497522+malyalarajasekhar@users.noreply.github.com>

--- Process_2/process2.py
+++ Process_2/process2.py
@@ -3,3 +3,9 @@
 =======
 ####minikubeminikubeminikubeminikubeminikubeminikubeminikubeminikubeminikubeminikube
 
+
+Require status checks before merging
+Require status checks before merging
+Require status checks before merging
+Require status checks before merging
+Require status checks before merging
